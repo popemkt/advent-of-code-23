efd762b8225a3edc2cfcaa639051aacdc4e04adf
Nguyen Gia Hoang <popemkt1@gmail.com>
2023-12-08 22:50:57 +0700

    Added day 1 + 2

---
+++ day-1/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//URL: https://adventofcode.com/2023/day/1
+
+var input = File.ReadAllLines("input.txt");
+HashSet<char> numbers = new() { '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+(string Text, int Value)[] numbersAsString =
+{
+    ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9),
+};
+HashSet<char> potentialNumber = numbersAsString.Select(x => x.Text[0]).ToHashSet();
+
+var sum = 0;
+foreach (var line in input)
+{
+    int? first = null;
+    int? last = null;
+    var span = line.AsSpan();
+    var lineLength = span.Length;
+    for (var i = 0; i < span.Length; i++)
+    {
+        var character = span[i];
+        if (numbers.Contains(character))
+        {
+            if (first.HasValue)
+                last = Int32.Parse(character.ToString());
+            else
+                first = Int32.Parse(character.ToString());
+        }
+        else if (potentialNumber.Contains(character))
+        {
+            foreach (var number in numbersAsString)
+            {
+                if (number.Text.AsSpan().SequenceEqual(span.Slice(i, Math.Min(lineLength - i, number.Text.Length))))
+                {
+                    if (first.HasValue)
+                        last = number.Value;
+                    else
+                        first = number.Value;
+                    break;
+                }
+            }
+        }
+    }
+
+    sum += 10 * first.Value + (last ?? first.Value);
+}
+
+Console.WriteLine(sum);
---
+++ day-2/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const string RED = "red";
+const string BLUE = "blue";
+const string GREEN = "green";
+var colors = new[] { RED, BLUE, GREEN };
+
+var input = File.ReadAllLines("input.txt");
+
+var sumPossibleGameId = 0;
+var sumPowerOfMinSet = 0;
+var criteria = new Dictionary<string, int>()
+{
+    { RED, 12 },
+    { BLUE, 14 },
+    { GREEN, 13 }
+};
+foreach (var game in input)
+{
+    var possible = true;
+    var gameSplit = game.Split(":");
+    var gameNum = Int32.Parse(gameSplit[0].Split(" ")[1]);
+    var sets = gameSplit[1].Trim().Split(";");
+
+    var minGameScore = new Dictionary<string, int>()
+    {
+        { RED, 0 },
+        { BLUE, 0 },
+        { GREEN, 0 }
+    };
+
+    foreach (var set in sets)
+    {
+        var setScore = new Dictionary<string, int>()
+        {
+            { RED, 0 },
+            { BLUE, 0 },
+            { GREEN, 0 }
+        };
+        var turns = set.Split(",");
+
+        //Calculate score
+        foreach (var turn in turns)
+        {
+            var turnSplit = turn.Trim().Split(" ");
+            var num = Int32.Parse(turnSplit[0]);
+            var color = turnSplit[1];
+            setScore[color] += num;
+            
+            minGameScore[color] = Math.Max(minGameScore[color], setScore[color]);
+        }
+
+        //Check validity
+        foreach (var color in colors)
+        {
+            if (setScore[color] > criteria[color])
+                possible = false;
+        }
+    }
+    
+    var setPower = minGameScore.Values.Aggregate(1, (current, next) => current * next);
+    sumPowerOfMinSet += setPower;
+
+    if (possible)
+        sumPossibleGameId += gameNum;
+}
+
+Console.WriteLine("{0}", sumPossibleGameId);
+Console.WriteLine("{0}", sumPowerOfMinSet);
